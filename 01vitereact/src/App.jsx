a8a612914c845eee66a9954e131b5c8012acadb9
darpanhh <darpangiri5@gmail.com>
2025-12-18 23:04:02 +0545

    React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from './chai'
+
+function App() {
+  const username ='darpan giri'
+
+  return (
+    <>
+      <h1>Hello World with Vite {username}</h1>
+      <Chai/>
+      <p>test para</p>
+    </>
+   
+  )
+}
+
+export default App
