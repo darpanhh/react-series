04311a133033c799a55da44890df3100253c4705
darpanhh <darpangiri5@gmail.com>
2026-01-10 22:40:09 +0545

    Rdux toolkit

---
+++ reduxToolkittodo/src/features/todo/todoSlice.js
@@ -0,0 +1,21 @@
+import {createSlice,nanoid} from '@reduxjs/toolkit';
+
+const initialState = {
+    todos:[{id:1,text:"Hello world"}]
+}
+
+
+export const todoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo: (state,action)=>{
+            const todo = {
+                id:nanoid(),
+                text:action.payload
+            }
+        },
+        removeTodo: ()=>{},
+
+    }
+})
