99e92fc02a075c397500101c87783b6023e20953
darpanhh <darpangiri5@gmail.com>
2026-01-18 21:04:17 +0545

    Practice

---
+++ router practice/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+function App() {
+  return (
+    <>
+    <h1>Learn about redux toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
